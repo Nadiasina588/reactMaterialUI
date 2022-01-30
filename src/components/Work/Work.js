49a42572edb80ee7336b2e2e519112c65c17e252
NadiasinaNico <nadiasinanicodev@gmail.com>
2022-01-31 00:49:36 +0300

    refonte

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -4,7 +4,8 @@ import Image from "../../image/image.png";
 
 import Portfolio from "../../image/portfolio.png";
 import Maka from "../../image/voiture.png";
-
+import Etablissement from "../../image/gestion-etablissement-public.png";
+import Pays from "../../image/api-continent.png";
 const Work = () => {
   return (
     <div>
@@ -29,10 +30,16 @@ const Work = () => {
                 <p class="text_work_p">Made with React JS </p>
                 <div className="box_code">
                   <div className="box_code_source">
-                    <a className="btn_box_code_source" href="https://github.com/NadiasinaNico/reactMaterialUI">code source</a>
+                    <a
+                      className="btn_box_code_source"
+                      target="_blank"
+                      href="https://github.com/NadiasinaNico/reactMaterialUI"
+                    >
+                      code source
+                    </a>
                   </div>
                   <div className="box_code_lien">
-                    <a className="btn_box-code_lien" href="">
+                    <a className="btn_box-code_lien" href="" target="_blank">
                       regarder l'application
                     </a>
                   </div>
@@ -47,11 +54,16 @@ const Work = () => {
                 <p class="text_work_p">Made with Symfony</p>
                 <div className="box_code">
                   <div className="box_code_source">
-                    <a className="btn_box_code_source" 
-                    href="https://github.com/NadiasinaNico/gestion-des-voitures">code source</a>
+                    <a
+                      className="btn_box_code_source"
+                      href="https://github.com/NadiasinaNico/gestion-des-voitures"
+                      target="_blank"
+                    >
+                      code source
+                    </a>
                   </div>
                   <div className="box_code_lien">
-                    <a className="btn_box-code_lien" href="">
+                    <a className="btn_box-code_lien" href="" target="_blank">
                       regarder l'application
                     </a>
                   </div>
@@ -60,15 +72,40 @@ const Work = () => {
             </div>
             <div className="card box">
               <div className="box">
-                <img src={Maka} alt="" className="image_work" />
-                <div className="text_work">Gestion vente de zebu</div>
-                <p class="text_work_p">Made with Symfony.</p>
+                <img src={Etablissement} alt="" className="image_work" />
+                <div className="text_work">Gestion etablissement public</div>
+                <p class="text_work_p">Made with React JS with API</p>
                 <div className="box_code">
                   <div className="box_code_source">
-                    <a className="btn_box_code_source" href="">code source</a>
+                    <a
+                      className="btn_box_code_source"
+                      href="https://github.com/NadiasinaNico/gestion-des-voitures"
+                      target="_blank"
+                    >
+                      code source
+                    </a>
                   </div>
                   <div className="box_code_lien">
-                    <a className="btn_box-code_lien" href="">
+                    <a className="btn_box-code_lien" href="" target="_blank">
+                      regarder l'application
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card box">
+              <div className="box">
+                <img src={Pays} alt="" className="image_work" />
+                <div className="text_work">Gestion Pays</div>
+                <p class="text_work_p">Made with React JS with API.</p>
+                <div className="box_code">
+                  <div className="box_code_source">
+                    <a className="btn_box_code_source" href="" target="_blank">
+                      code source
+                    </a>
+                  </div>
+                  <div className="box_code_lien">
+                    <a className="btn_box-code_lien" href="" target="_blank">
                       regarder l'application
                     </a>
                   </div>
